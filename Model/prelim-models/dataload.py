45150824cdc7c293b734c2903f4097a6d666ff6e
MKSwaminathan <meenakshi.swaminathan@utexas.edu>
2020-04-04 17:01:14 -0500

    adding dataload api

---
+++ Model/prelim-models/dataload.py
@@ -0,0 +1,18 @@
+import os, sys
+import numpy as np
+import pandas as pd
+
+
+#
+# Inputs:
+#   list of csv files
+#   list of column headers
+#
+# Returns:
+#   single numpy array with all data
+#   for preprocessing
+#
+
+def make_predata(csv_files, column_headers):
+    pass
+
