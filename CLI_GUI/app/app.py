184f048cf749166382d4c83cc14590ac78147e65
Shehryar Ahmed <shehryar.ahmed@utexas.edu>
2020-04-28 23:23:32 -0500

    webapp works now, need to style

--- CLI_GUI/app/app.py
+++ CLI_GUI/app/app.py
@@ -11,12 +11,20 @@ import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 import PIL 
+from torch import tensor
 #import '../../Model/prelim-models/all-models/demo-for-shehryar'
 # from torch import Image
 # from torch import loader
 #from torchvision import Variable
+from torch.autograd import Variable
+from torch.utils import data
+from torchvision import datasets, transforms, models
+import io
+import json
+
 
 app = Flask(__name__)
+
 
 # @app.route('/')
 # def hello_world(): #check if app is running
@@ -29,23 +37,35 @@ def page():
 
 
 @app.route('/upload',methods=['POST']) #use this to upload images to compress
-def upload_image():
+def get_compression_level():
     try:
         nn_weights = '../../Model/prelim-models/all-models/demo-for-shehryar'
-        NN = load_neural_net(nn_weights)
-
-       
+        device = get_device()
+        
+        model = torch.load('../../Model/prelim-models/all-models/demo-for-shehryar', map_location=device)
         
         print(request.files) #show files that user uploaded
         num_images = 0  #number of images in request
         predicted_images = []   #will store each image uri in this var
+        compression_levels = []
         for i in request.files:
+            file_storage_obj = request.files[i]
+            # with open(file_storage_obj,'r'):
+            #     print(file_storage_obj.read())
             num_images += 1
-            # print(request.files[i]) 
-            nn_predicted_image = pred_image(NN, request.files[i]) #predict compression rate given neural net and image
-            print(nn_predicted_image)
-            img_str = base64.b64encode(nn_predicted_image.read())
-            predicted_images.append(img_str.decode('utf-8'))#append to list
+            img_str = io.BytesIO(file_storage_obj.read())#base64.b64encode(file_storage_obj.read())
+            print('i: ',img_str) 
+
+            img = load_image(img_str,device)
+            label = model(img)
+            print(label)
+            compression_level = label.detach().numpy().reshape(1,)[0]
+            print(compression_level)
+            #nn_predicted_image = pred_image(NN, request.files[i],device) #predict compression rate given neural net and image
+            compression_levels.append(compression_level)
+            #print(nn_predicted_image)
+            #img_str = base64.b64encode(nn_predicted_image.read())
+            #predicted_images.append(img_str.decode('utf-8'))#append to list
 
             # with open('cashmoney.jpg','rb') as img: #these 3 lines are just temporary, will return a random image
             #     img_str = base64.b64encode(img.read())
@@ -54,11 +74,11 @@ def upload_image():
         #KEEP THE LINE BELOW COMMENTED
         #model_conv.load_state_dict(torch.load(os.path.join(os.getcwd(),"KonIQ_model"),map_location=torch.device('cpu')))
 
-        print(len(predicted_images))
-        return jsonify(
-            num_images = num_images,
-            image_strings = predicted_images
-        ) 
+        print(compression_levels)
+        return json.dumps(str(compression_levels))
+        # return jsonify(
+        #     compression_levels = str(compression_levels)
+        # ) 
     except Exception as e:
         print(e)
         return str(e)
@@ -66,31 +86,50 @@ def upload_image():
 
 
 #these 3 functions 
-def load_neural_net(filename):
+def get_device():
+    #torch.nn.Module.dump_patches=True
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = torch.load(filename, map_location=device)
-    #n = torch.load(filename)
-    return model
-        
-def pred_image(NN,image):
-    image = image_loader(image)
-    return image
 
-def image_loader(image_name):
-    try:
-        """load image, returns cuda tensor"""
-        imsize = 500*375
-        loader = transforms.Compose([transforms.Resize(imsize), transforms.ToTensor()])
+    #model = torch.load(filename, map_location=device)
+    
+    return device
         
-        image = Image.open(image_name)
-        print(image)
-        image = loader(image).float()
-        #image = Variable(image, requires_grad=True)
-        #image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
+def load_image(img,device):
+    imsize = 256
+    loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
+    image = Image.open(img)
+    image = loader(image).float()
+    image = Variable(image, requires_grad=True)
+    image = image.unsqueeze(0)
+    return image.to(device) 
+
+# def pred_image(NN,image,device):
+#     image = image_loader(image,device)
+#     label = model(image)
+#     compression_level = label.detach().numpy().reshape(1,)[0]
+
+#     return compression_level
+
+# def image_loader(image_name,device):
+#     try:
+#         """load image, returns cuda tensor"""
+
+#         imsize =256
+#         loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
         
-        return image.cpu()#image.cuda()  #assumes that you're using GPU
-    except Exception as e:
-        print(e)
+#         image = Image.open(image_name)
+#         print(image)
+#         image = Variable(image, requires_grad=True)
+#         image = image.unsqueeze(0)
+#         return image.to(device) 
+#         #image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
+        
+#         #return image.cpu()#image.cuda()  #assumes that you're using GPU
+#     except Exception as e:
+#         print(e)
+
+# nn_weights = '../../Model/prelim-models/all-models/demo-for-shehryar'
+# NN,device = load_neural_net(nn_weights)
 
 if __name__ == "__main__":
     app.run(debug=True)
