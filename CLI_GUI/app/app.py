d3ce42fe9d718f3b55ae5cd9d12f02db69a4e70f
Shehryar Ahmed <shehryar.ahmed@utexas.edu>
2020-04-28 19:30:19 -0500

    adding model to webapp

--- CLI_GUI/app/app.py
+++ CLI_GUI/app/app.py
@@ -8,18 +8,22 @@ import numpy as np
 from flask import jsonify
 import torch
 import torch.nn as nn
-#from torch import Image
-#from torch import loader
-#from torch import Variable
+from PIL import Image
+from torchvision import transforms
+import PIL 
+#import '../../Model/prelim-models/all-models/demo-for-shehryar'
+# from torch import Image
+# from torch import loader
+#from torchvision import Variable
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world(): #check if app is running
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world(): #check if app is running
+#     return 'Hello, World!'
 
 
-@app.route('/page') #this returns the html page
+@app.route('/') #this returns the html page
 def page():
     return render_template('upload_image.html')
 
@@ -27,8 +31,8 @@ def page():
 @app.route('/upload',methods=['POST']) #use this to upload images to compress
 def upload_image():
     try:
-        nn_weights = 'temp.txt'
-        #\NN = load_neural_net(nn_weights)
+        nn_weights = '../../Model/prelim-models/all-models/demo-for-shehryar'
+        NN = load_neural_net(nn_weights)
 
        
         
@@ -37,14 +41,15 @@ def upload_image():
         predicted_images = []   #will store each image uri in this var
         for i in request.files:
             num_images += 1
-            #print(request.files[i]) 
-            #nn_predicted_image = pred_image(NN, request.files[i]) #predict compression rate given neural net and image
-            # img_str = base64.b64encode(nn_predicted_image.read())
-            # predicted_images.append(img_str.decode('utf-8'))#append to list
+            # print(request.files[i]) 
+            nn_predicted_image = pred_image(NN, request.files[i]) #predict compression rate given neural net and image
+            print(nn_predicted_image)
+            img_str = base64.b64encode(nn_predicted_image.read())
+            predicted_images.append(img_str.decode('utf-8'))#append to list
 
-            with open('cashmoney.jpg','rb') as img: #these 3 lines are just temporary, will return a random image
-                img_str = base64.b64encode(img.read())
-                predicted_images.append(img_str.decode('utf-8'))
+            # with open('cashmoney.jpg','rb') as img: #these 3 lines are just temporary, will return a random image
+            #     img_str = base64.b64encode(img.read())
+            #     predicted_images.append(img_str.decode('utf-8'))
 
         #KEEP THE LINE BELOW COMMENTED
         #model_conv.load_state_dict(torch.load(os.path.join(os.getcwd(),"KonIQ_model"),map_location=torch.device('cpu')))
@@ -62,20 +67,30 @@ def upload_image():
 
 #these 3 functions 
 def load_neural_net(filename):
-    n = torch.load(filename)
-    return n
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = torch.load(filename, map_location=device)
+    #n = torch.load(filename)
+    return model
         
 def pred_image(NN,image):
     image = image_loader(image)
     return image
 
 def image_loader(image_name):
-    """load image, returns cuda tensor"""
-    image = Image.open(image_name)
-    image = loader(image).float()
-    image = Variable(image, requires_grad=True)
-    #image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
-    return image.cuda()  #assumes that you're using GPU
+    try:
+        """load image, returns cuda tensor"""
+        imsize = 500*375
+        loader = transforms.Compose([transforms.Resize(imsize), transforms.ToTensor()])
+        
+        image = Image.open(image_name)
+        print(image)
+        image = loader(image).float()
+        #image = Variable(image, requires_grad=True)
+        #image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
+        
+        return image.cpu()#image.cuda()  #assumes that you're using GPU
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     app.run(debug=True)
