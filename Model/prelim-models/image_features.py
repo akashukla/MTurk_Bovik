d7f1473eb9030f3c8c488a352d68a4999218b705
Akash Shukla <akashukla@utexas.edu>
2020-04-28 20:25:13 -0500

    new image features

--- Model/prelim-models/image_features.py
+++ Model/prelim-models/image_features.py
@@ -6,11 +6,15 @@ import cv2
 import matplotlib as mpl
 mpl.rcParams['lines.markersize']=1.0
 
+bins1=np.load('bins1.npy')
+bins1=np.load('bins2.npy')
+
 def gen_batch_data():
     batch_files=glob.glob('batch-data/batch*')
     hit_files=glob.glob('batch-data/hit*')
     batch_dfs = []
     batch_setnums = []
+    batch_srccs= []
     batch_svals = []
     batch_svals2 = []
     
@@ -19,6 +23,10 @@ def gen_batch_data():
     batch_setnums_arr=np.array([], dtype='int')
     batch_image_names_arr= np.array([], dtype='str')
     batch_workers_arr= np.array([], dtype='str')
+    batch_srccs_arr=np.array([], dtype='float64')
+    batch_binvals1_arr=np.array([], dtype='int')
+    batch_binvals2_arr=np.array([], dtype='int')
+
     
     hit_data_orig = np.genfromtxt('batch-data/hit_data_orig.csv',delimiter=',',dtype='str')[:,1:56]
     hit_data_2_electric_boogaloo= np.genfromtxt('batch-data/hit_data_2_electric_boogaloo.csv',delimiter=',',dtype='str')[:,1:56]
@@ -35,17 +43,18 @@ def gen_batch_data():
         batch_dfs.append(pd.read_csv(batch_files[i]))
         batch_dfs[i] = batch_dfs[i][batch_dfs[i].loc[:,'AssignmentStatus']=='Approved']
         batch_dfs[i] = batch_dfs[i][batch_dfs[i].loc[:,'Answer.set_number']!='initial']
-    
         batch_setnums.append(batch_dfs[i].loc[:,'Answer.set_number'].values.astype('int'))
-    
-        #batch_svals.append([np.fromstring(np.array(batch_dfs[i].loc[:,'Answer.slider_values'])[j],dtype='int',sep=',') for j in range(len(batch_dfs[i]))])
         batch_svals.append([np.fromstring(batch_dfs[i].loc[:,'Answer.slider_values'].values[j],dtype='int',sep=',') for j in range(len(batch_dfs[i]))])
         batch_svals2.append([np.fromstring(batch_dfs[i].loc[:,'Answer.slider_values2'].values[j],dtype='int',sep=',') for j in range(len(batch_dfs[i]))])
+        batch_srccs.append(batch_dfs[i].loc[:,'Answer.srcc_score'].values.astype('float64'))
     
         batch_svals_arr = np.append(batch_svals_arr,np.array(batch_svals[i]))
         batch_svals2_arr = np.append(batch_svals2_arr,np.array(batch_svals2[i]))
         batch_setnums_arr = np.append(batch_setnums_arr,batch_setnums[i])
         batch_workers_arr= np.append(batch_workers_arr,np.repeat(batch_dfs[i].loc[:,'WorkerId'].values.astype('str'), 55) )
+        batch_srccs_arr= np.append(batch_srccs_arr,batch_srccs[i])
+        batch_binvals1_arr = np.append(batch_binvals1_arr, np.digitize(np.array(batch_svals[i]), bins1))
+        batch_binvals2_arr = np.append(batch_binvals2_arr, np.digitize(np.array(batch_svals2[i]), bins2))
     
         if i in orig_ind:
             batch_image_names_arr = np.append(batch_image_names_arr,hit_data_orig[batch_setnums[i]%len(hit_data_orig)-1])
@@ -62,8 +71,11 @@ def gen_batch_data():
     #result is batch_image_names_arr, batch_svals_arr,batch_svals2_arr, batch_workers_arr
     np.save('batch_image_names_arr', batch_image_names_arr)
     np.save('batch_svals_arr', batch_svals_arr)
+    np.save('batch_srccs_arr', batch_srccs_arr)
     np.save('batch_svals2_arr', batch_svals2_arr)
     np.save('batch_workers_arr', batch_workers_arr)
+    np.save('batch_binvals1_arr', batch_binvals1_arr)
+    np.save('batch_binvals2_arr', batch_binvals2_arr)
     return batch_image_names_arr, batch_svals_arr, batch_svals2_arr, batch_workers_arr
 
 def image_features(image):
@@ -108,22 +120,39 @@ def variance_of_laplacian(image):
 
 
 image_names = np.load('batch_image_names_arr.npy')
-#svals = np.load('batch_svals_arr.npy')
-#svals2 = np.load('batch_svals2_arr.npy')
+svals1 = np.load('batch_svals_arr.npy')
+svals2 = np.load('batch_svals2_arr.npy')
 workers = np.load('batch_workers_arr.npy')
+binvals1, binvals2 = np.load('batch_binvals1_arr.npy'), np.load('batch_binvals2_arr.npy')
+uq_workers=np.unique(workers)
+group1_workers = workers[:len(workers)//2]
+group2_workers = workers[len(workers)//2:-1]
 
 uq_image, uq_ind, uq_count = np.unique(image_names,return_index=True, return_counts=True)
 
-
 image_svals = np.zeros((uq_image.shape[0], 2), dtype='float64')
+image_svals_g1 = np.zeros((uq_image.shape[0], 2), dtype='float64')
+image_svals_g2 = np.zeros((uq_image.shape[0], 2), dtype='float64')
 
 image_feats = np.zeros((uq_image.shape[0], 5),dtype='float64')
+
+image_binvals = np.zeros((uq_image.shape[0], 2), dtype='int')
+image_binvals_g1 = np.zeros((uq_image.shape[0], 2), dtype='int')
+image_binvals_g2 = np.zeros((uq_image.shape[0], 2), dtype='int')
+
 def gen_svals_features():
     i=0
     for uqi in uq_image:
-        if i%100==0:
+        if i%20==0:
             print(i)
-    ##    image_svals[i]= np.array([np.mean(svals[image_names==uqi]), np.mean(svals2[image_names==uqi])])
+        image_binvals[i] = np.mean(binvals2[image_names==uqi]), np.mean(binvals2[image_names==uqi])
+        image_binvals_g1[i] = np.mean(binvals1[image_names==uqi][np.in1d(workers[image_names==uqi], group1_workers)]), np.mean(binvals2[image_names==uqi][np.in1d(workers[image_names==uqi], group1_workers)]) 
+        image_binvals_g2[i] = np.mean(binvals1[image_names==uqi][np.in1d(workers[image_names==uqi], group2_workers)]), np.mean(binvals2[image_names==uqi][np.in1d(workers[image_names==uqi], group2_workers)]) 
+
+
+        image_svals[i]= np.mean(svals1[image_names==uqi]), np.mean(svals2[image_names==uqi])
+        image_svals_g1[i] = np.mean(svals1[image_names==uqi][np.in1d(workers[image_names==uqi], group1_workers)]), np.mean(svals2[image_names==uqi][np.in1d(workers[image_names==uqi], group1_workers)]) 
+        image_svals_g2[i] = np.mean(svals1[image_names==uqi][np.in1d(workers[image_names==uqi], group2_workers)]), np.mean(svals2[image_names==uqi][np.in1d(workers[image_names==uqi], group2_workers)]) 
 
     ##       #sv1 = batch_svals_arr[batch_image_names_arr==uqi] 
     ##       #sv2 = batch_svals2_arr[batch_image_names_arr==uqi]
@@ -136,12 +165,29 @@ def gen_svals_features():
     ##       #g1_s2[i] = sv2[0:sv2.shape[0]//2].mean()
     ##       #g2_s2[i] = sv2[sv2.shape[0]//2:].mean()
     
-        imi=cv2.imread('../data/8k_data/'+uqi)
-        image_feats[i] = image_features(imi)
+        #imi=cv2.imread('../data/8k_data/'+uqi)
+        #image_feats[i] = image_features(imi)
         i+=1
 
-    np.save('image_feats', image_feats)
-    #np.save('image_svals', image_svals)
+    #np.save('image_feats', image_feats)
+    np.save('image_svals', image_svals)
+    np.save('image_binvals', image_svals)
+    np.save('image_binvals_g1', image_binvals_g1)
+    np.save('image_binvals_g2', image_binvals_g1)
+    np.save('image_svals_g1', image_binvals_g1)
+    np.save('image_svals_g2', image_binvals_g1)
 
-#image_feats=np.load('image_feats.npy')
+image_feats=np.load('image_feats.npy')
 image_svals=np.load('image_svals.npy')
+#image_svals=np.load('image_binvals.npy')
+srccs = np.load('batch_srccs_arr.npy')
+
+
+sv1,sv2 = image_svals[:,0], image_svals[:,1]
+range1, range2 = sv1.max() - sv1.min(), sv2.max()-sv2.min()
+binsize1, binsize2 = range1/5, range2/5
+bins1, bins2 = sv1.min()+binsize1*np.r_[1:6:1], sv2.min()+binsize2*np.r_[1:6:1]
+binned_sv1, binned_sv2 = np.digitize(sv1,bins1,right=True), np.digitize(sv2, bins2, right=True)
+
+#np.save('bins1', bins1)
+#np.save('bins2', bins2)
